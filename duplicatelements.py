cd63b814b1f30797de4ee4f1bd93bee4daae8ab8
Naman Aggarwal <91172098+142002naman@users.noreply.github.com>
2022-01-12 21:03:08 +0530

    Added the programs question name for reference

--- duplicatelements.py
+++ duplicatelements.py
@@ -1,9 +1,12 @@
+'''Program to input no.s in list and find duplicate elements from the list'''
+
 a=[]
 b=[]
 e=[]
 for i in range(0,10,1):
     d=int(input("Enter Number:"))
     a.append(d)
+print("Duplicate elements from list are as following:")
 for r in a:
     if(r not in b):
         b.append(r)
